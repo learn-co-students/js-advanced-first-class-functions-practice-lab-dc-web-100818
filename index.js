89d00e4a6420556675c2612212609893710a37d2
Solee Lee <carolinesoleelee@gmail.com>
2018-11-19 20:36:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function logDriverNames(drivers){
+  drivers.forEach(function(driver) {
+    console.log(driver.name);
+  })
+}
+
+function logDriversByHometown(drivers, location) {
+  const array = [...drivers]
+   array.forEach(function(driver) {
+    if(driver.hometown === location)
+    console.log(driver.name);
+  })
+}
+
+function driversByRevenue(drivers){
+   const newdrivers = [...drivers]
+    return newdrivers.sort(function(a, b){
+    return a.revenue - b.revenue;
+   })
+}
+
+function driversByName(drivers) {
+  const newdrivers = [...drivers]
+  return newdrivers.sort(function(a, b) {
+    return (a.name).localeCompare(b.name);
+  })
+}
+
+function totalRevenue(drivers){
+  let total = 0
+  drivers.map(driver => total += driver.revenue)
+  return total
+}
+
+function averageRevenue(drivers){
+  return totalRevenue(drivers) / drivers.length;
+}
